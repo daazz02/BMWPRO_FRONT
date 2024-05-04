c8883e187e553153687e0c7430a114e2d3a4386e
Dmytro <dima06444@gmail.com>
2024-05-04 16:21:31 +0300

    feat: create basic browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import bmwImg from "./assets/4k-bmw-m4-f82-drift-bmw-m.jpg";
 import bmwNav from "./assets/sticker.webp";
 import bmwu from "./assets/user.png";
@@ -7,44 +6,36 @@ import bmwm from "./assets/man.png";
 import bmws from "./assets/spa.png";
 import bmwt from "./assets/tele.png";
 
-
-
-
-
-
 function App() {
   return (
-      <div>
-        <div className="navigationWrapper">
-          <nav className="nav">
-            <ul className="navigation">
-              <li className="bmwNav">
-                <img src={bmwNav} alt="bmw" width={40}/>
-              </li>
-              <li className="bmwt">
-                <img src={bmwt} alt="bmw" width={27}/>
-              </li>
-              <li className="bmws">
-                <img src={bmws} alt="bmw" width={27}/>
-              </li>
-              <li className="bmwm">
-                <img src={bmwm} alt="bmw" width={27}/>
-              </li>
-              <li className="bmwl">
-                <img src={bmwl} alt="bmw" width={27}/>
-              </li>
-              <li className="bmwu">
-                <img src={bmwu} alt="bmw" width={25}/>
-              </li>
-
-            </ul>
-          </nav>
-          <img src={bmwImg} alt="bmw" className="banner"/>
-        </div>
+    <div>
+      <div className="navigationWrapper">
+        <nav className="nav">
+          <ul className="navigation">
+            <li className="bmwNav">
+              <img src={bmwNav} alt="bmw" width={40} />
+            </li>
+            <li className="bmwt">
+              <img src={bmwt} alt="bmw" width={27} />
+            </li>
+            <li className="bmws">
+              <img src={bmws} alt="bmw" width={27} />
+            </li>
+            <li className="bmwm">
+              <img src={bmwm} alt="bmw" width={27} />
+            </li>
+            <li className="bmwl">
+              <img src={bmwl} alt="bmw" width={27} />
+            </li>
+            <li className="bmwu">
+              <img src={bmwu} alt="bmw" width={25} />
+            </li>
+          </ul>
+        </nav>
+        <img src={bmwImg} alt="bmw" className="banner" />
       </div>
+    </div>
   );
 }
-
-
 
 export default App;
