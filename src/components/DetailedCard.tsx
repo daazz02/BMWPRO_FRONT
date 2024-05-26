d9ba8581ff646afa007b0f6a2aff6b4783495a48
Dmytro <dima06444@gmail.com>
2024-05-26 17:03:55 +0300

    feat: add  a basic detailed card

---
+++ src/components/DetailedCard.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  // Define your component's props here
+}
+
+const DetailedCard: React.FC<Props> = (props) => {
+  // Implement your component logic here
+
+  return (
+    <div className="bg-[#1D63A2] border border-black shadow-md rounded-[67px] w-[298px] h-[374px]">
+      Detailed
+    </div>
+  );
+};
+
+export default DetailedCard;
--- src/pages/Motorbike.tsx
+++ src/pages/Motorbike.tsx
@@ -1,5 +1,18 @@
+import { Fragment } from "react/jsx-runtime";
+import DetailedCard from "../components/DetailedCard";
+
 function Motorbike() {
-  return <div className="mt-20">Motorbike</div>;
+  return (
+    <div className="mt-20">
+      <div className="flex gap-5 flex-wrap">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <Fragment key={index}>
+            <DetailedCard />
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Motorbike;
