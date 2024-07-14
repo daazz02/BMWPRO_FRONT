bed8d3dc5205e230b452469ca565b51ebab2adc9
daazz02 <146915613+daazz02@users.noreply.github.com>
2024-07-14 15:57:34 +0300

    motorbike_cards

--- src/components/DetailedCard.tsx
+++ src/components/DetailedCard.tsx
@@ -15,27 +15,40 @@ const DetailedCard: React.FC<Props> = ({ index, data }) => {
   };
 
   return (
-    <div
-      className={`border border-black shadow-md rounded-[67px] w-[298px] h-[374px]`}
-      style={{ backgroundColor }}
-    >
-      <h2 className="text-center mt-[14px] text-xl" style={{ color: getTextColor(index) }}>
-        {data.name}
-      </h2>
-      <img src={data.img} alt={data.name} className="w-[150px] h-[150px] mx-auto mt-[14px]" />
       <div
-        className={[
-          "mt-[14px] text-left ml-5 [&>p]:text-shadow",
-          index % 4 !== 3 && "shadow-black",
-        ].join(" ")}
-        style={{ color: getTextColor(index) }}
+          className={`border border-black shadow-md rounded-[67px] w-[298px] h-[392px] flex flex-col justify-between`}
+          style={{backgroundColor}}
       >
-        <p className="text-lg text-shadow">{data.rentDaily}$ in daily</p>
-        <p className="text-lg text-shadow-[0_4px_8px_#6366f1]">{data.rentWeekly}$ in weekly</p>
-        <p className="text-lg">{data.rentMonthly}$ in monthly</p>
-        <p className="text-lg">{data.rentYearly}$ in yearly</p>
+        <h2 className="text-center mt-[14px] text-xl" style={{color: getTextColor(index)}}>
+          {data.name}
+        </h2>
+        <div className="flex justify-center">
+          <img
+              src={data.img}
+              alt={data.name}
+              className="w-[150px] h-[150px] object-contain shadow-lg"
+          />
+        </div>
+        <div
+            className="text-left ml-5"
+            style={{color: getTextColor(index)}}
+        >
+          <p className="text-lg text-shadow-[0_4px_8px_#2f3030]">{data.rentDaily}$ in daily</p>
+          <p className="text-lg text-shadow-[0_4px_8px_#2f3030]">{data.rentWeekly}$ in weekly</p>
+          <p className="text-lg text-shadow-[0_4px_8px_#2f3030]">{data.rentMonthly}$ in monthly</p>
+          <p className="text-lg text-shadow-[0_4px_8px_#2f3030]">{data.rentYearly}$ in yearly</p>
+        </div>
+        <div className="flex justify-center mb-4">
+          <button
+              className="text-white w-[190px] h-[50px] rounded-full"
+              style={{
+                background: "linear-gradient(90deg, #090f30,#000000,#190631)"
+              }}
+          >
+            basket
+          </button>
+        </div>
       </div>
-    </div>
   );
 };
 
