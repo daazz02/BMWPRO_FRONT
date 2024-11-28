fb03f87e5663a0462c22ce49ad49b9a9124e8757
daazz02 <146915613+daazz02@users.noreply.github.com>
2024-11-28 19:12:38 +0200

    new_Support

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -4,18 +4,17 @@ const Support: React.FC = () => {
     return (
         <div className="bg-[url('/src/assets/logg.jpg')] bg-no-repeat bg-cover bg-center bg-fixed min-h-screen text-white flex items-center justify-center">
             <div className="text-center px-4 sm:px-6 lg:px-8 w-full">
-                {/* Центрированный заголовок */}
                 <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-8">
                     Support Page
                 </h1>
 
-                {/* Центрированные блоки с увеличенным размером */}
                 <div className="flex flex-wrap gap-8 justify-center">
-                    <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full sm:w-80 lg:w-96 text-center">
+                    <div className="bg-gradient-to-r from-pink-800 to-blue-800 p-8 rounded-lg shadow-lg w-full sm:w-80 lg:w-96 text-center">
                         <h2 className="text-2xl font-bold mb-4">FAQ Section</h2>
                         <p className="text-lg">+380993456780</p>
                     </div>
-                    <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full sm:w-80 lg:w-96 text-center">
+
+                    <div className="bg-gradient-to-r from-purple-900 to-orange-800 p-8 rounded-lg shadow-lg w-full sm:w-80 lg:w-96 text-center">
                         <h2 className="text-2xl font-bold mb-4">Contact Support</h2>
                         <p className="text-lg">+380667845637</p>
                     </div>
